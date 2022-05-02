7921fe3d95cccb5d3fa973ee837ab99327a50501
GuiSilva2022 <gui.carlossrs2018@gmail.com>
2022-05-01 22:45:59 -0300

    Js e HTML arrumados

--- lista.js
+++ lista.js
@@ -1,37 +1,40 @@
-function DeleteItem(){
-    console.log(this.parentElement)
+ function DeleteItem(){
     this.parentElement.remove()
 }
 
-function Edit(){
-    console.log(this.parentElement)
-    this.parentElement.editItem()
+ function EditItem(){
+    item_lista = this.parentElement
+    texto_item_lista  = item_lista.innerText.replace("DeleteEdit", "")
+    input_element = document.querySelector('[data-form-input]')
+    input_element.value = texto_item_lista
+    const botao_submit = document.querySelector('[button-submit]')
+    botao_submit.className = 'd-none'
+    const botao_edit = document.querySelector('[button-edit]')
+    botao_edit.classList.remove('d-none')
 
 }
 
-function CriarBotaoDelete(){
+ function CriarBotaoEdit(){
+    const botao_edit =  document.createElement('button')
+    botao_edit.classList.add("btn", "btn-info")
+    botao_edit.innerHTML = "Editar"
+    botao_edit.type = "button"
+    botao_edit.addEventListener("click", EditItem);
+
+    return botao_edit
+}
+
+ function CriarBotaoDelete(){
     const botao_delete =  document.createElement('button')
     botao_delete.classList.add("btn", "btn-danger")
-    botao_delete.innerHTML = "Apagar"
+    botao_delete.innerHTML = "Deletar"
     botao_delete.type = "button"
     botao_delete.addEventListener("click", DeleteItem);
 
     return botao_delete
 }
 
-
-function Editar(){
-    const botao_editar =  document.createElement('button')
-    botao_editar.classList.add("btn", "btn-info")
-    botao_editar.innerHTML = "Edit"
-    botao_editar.type = "button"
-    botao_editar.addEventListener("click", Edit);
-
-
-    return botao_editar
-}
-
-function Submit(){
+ function Submit(){
     const lista = document.querySelector('[data-task]')
     const valor = document.querySelector('[data-form-input]')
 
@@ -41,11 +44,29 @@ function Submit(){
     novo_item_lista.innerHTML = valor.value
  
     novo_item_lista.appendChild(CriarBotaoDelete())
-    novo_item_lista.appendChild(Editar())
+    novo_item_lista.appendChild(CriarBotaoEdit())
     lista.appendChild (novo_item_lista)
 
     document.getElementById("item").value = ""
+}
+
+ function Edit(){
     
+    texto_adicionado = input_element.value
+    item_lista.innerHTML = texto_adicionado
+
+    item_lista.appendChild(CriarBotaoDelete())
+    item_lista.appendChild(CriarBotaoEdit())
+
+    const botao_submit = document.querySelector('[button-submit]')
+    botao_submit.classList.remove('d-none')
+    botao_submit.classList.add('btn', 'btn-dark')
+
+    const botao_edit = document.querySelector('[button-edit]')
+    botao_edit.classList.add('d-none')
+
+    input_element.value = '';
+
 }
 
 
